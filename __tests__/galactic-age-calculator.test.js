27ff928dc59b88dd8eda2749b82d4e23aec6b3f7
Todd Walraven <cod4247@yahoo.com>
2020-02-07 08:49:34 -0800

    write first test for meaningful fail

---
+++ __tests__/galactic-age-calculator.test.js
@@ -0,0 +1,10 @@
+import { Age } from "../src/galactic-age-calculator";
+
+describe("Age", () => {
+
+  test("this should confirm the age of the user", () => {
+    let age = new Age(42, 0, 0, 0, 0)
+    expect(age.checkUserAge().toEqual(42))
+  })
+
+});
---
+++ src/galactic-age-calculator.js
@@ -0,0 +1,10 @@
+export class Age {
+  constructor(userAge, mercury, venus, mars, jupiter) {
+  this.userAge = userAge,
+  this.mercury = mercury, 
+  this.venus = venus, 
+  this.mars = mars
+  this.jupiter = jupiter
+  }
+  
+}
