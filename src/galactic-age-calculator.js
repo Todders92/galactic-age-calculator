e5e0946133dc72f7a83916ef270c1abcfbd434a7
Todd Walraven <cod4247@yahoo.com>
2020-02-10 18:58:09 -0800

    properly lint code

--- src/galactic-age-calculator.js
+++ src/galactic-age-calculator.js
@@ -1,15 +1,14 @@
-
 export class Age {
   constructor(userAge) {
     this.userAge = userAge,
     this.mercury = Math.round(userAge / .24), 
     this.venus = Math.round(userAge / .62), 
     this.mars = Math.round(userAge / 1.88),
-    this.jupiter = Math.round(userAge / 11.86)
+    this.jupiter = Math.round(userAge / 11.86);
   }
   checkUserAge(age) {
     if (age >=1 && age < 131)
-    return age
+      return age;
   } 
   
   earthLife(age) {
@@ -22,16 +21,6 @@ export class Age {
     }
   }
 
-  mercuryLife(age) {
-    const mercAge = parseFloat((age /.24).toFixed(2));
-    const lifeSpan = 325;
-    const mercuryExceed =  mercAge - lifeSpan;
-    if (lifeSpan >= mercAge) {
-      return mercuryExceed.toFixed(2);
-    } else{
-      return parseFloat((mercAge - lifeSpan).toFixed(2));
-    }
-  }
   mercuryLife(age) {
     const mercAge = parseFloat((age /.24).toFixed(2));
     const lifeSpan = 325;
@@ -72,4 +61,4 @@ export class Age {
       return parseFloat((jupiterAge - lifeSpan).toFixed(2));
     }
   }
-};
+}
